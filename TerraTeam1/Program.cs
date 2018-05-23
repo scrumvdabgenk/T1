5e43ef7ec5a725cf00ac4a51194492321c4cdfd1
KdC <zi24@L120-PC09>
2018-05-23 15:34:36 +0200

    push on MasterKdC

--- TerraTeam1/Program.cs
+++ TerraTeam1/Program.cs
@@ -10,12 +10,6 @@ namespace TerraTeam1
     {
         static void Main(string[] args)
         {
-            /* test */
-            /* test 2 */
-            /* test x*/
-            /*
-             * by KdC 
-            */
 
         }
     }
