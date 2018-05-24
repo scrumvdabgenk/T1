95fa5730647e15d160b4353da20ed0afb41f3f43
joballet01 <joballet01@gmail.com>
2018-05-24 08:59:29 +0200

    dier aangemaakt

---
+++ TerraTeam1/Dier.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerraTeam1
+{
+    abstract class Dier
+    {
+        public Dier()
+        {
+
+        }
+        private int nameValue;
+
+        public int Name
+        {
+            get { return nameValue; }
+            set { nameValue = value; }
+        }
+
+        private int bewegingValue;
+
+        public int Beweging
+        {
+            get { return bewegingValue; }
+            set { bewegingValue = value; }
+        }
+
+        private int etenValue;
+
+        public int Eten
+        {
+            get { return etenValue; }
+            set { etenValue = value; }
+        }
+
+        private int totaantStappenValue;
+
+        public int TotAantStappen
+        {
+            get { return totaantStappenValue; }
+            set { totaantStappenValue = value; }
+        }
+
+
+
+
+
+    }
+}
