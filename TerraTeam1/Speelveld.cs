219cbfee6c735f26c6604ae559aaf79754c2163a
KdC <zi24@L120-PC09>
2018-05-24 11:14:59 +0200

    Aanmaken SpeelveldTEST

---
+++ TerraTeam1/Speelveld.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerraTeam1
+{
+    public class Speelveld
+    {
+        //Creatie van een dynamic array voor speelveld
+        IOrganismen[,] Terrarium = null;
+
+        //Aanmaken van de grootte values voor X en Y
+        private int grootteXval;
+        private int grootteYval;
+        public int GrootteX
+        {
+            get { return grootteXval; }
+            set { grootteXval = value; }
+        }
+        public int GrootteY
+        {
+            get { return grootteYval; }
+            set { grootteYval = value; }
+        }
+
+        //Aanpassen van het speelveld aan de specificaties opgegeven in de constructor
+        public Speelveld(int GrootteX, int GrootteY)
+        {
+            this.grootteXval = GrootteX;
+            this.grootteYval = GrootteY;
+            Terrarium = new IOrganismen[GrootteX, GrootteY];
+        }
+
+    }
+}
---
+++ UnitTestTerraTeam1/UnitTestSpeelveld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TerraTeam1;
+
+namespace UnitTestTerraTeam1
+{
+    [TestClass]
+    public class UnitTestSpeelveld
+    {
+        [TestMethod]
+        public void TestSpeelveld()
+        {
+            Speelveld testVeld = new Speelveld(5,4);
+            Assert.AreEqual(testVeld.GrootteX, 5);
+            Assert.AreEqual(testVeld.GrootteY, 4);
+        }
+    }
+}
