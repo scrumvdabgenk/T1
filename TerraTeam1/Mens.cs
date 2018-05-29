3f6350580b0993e3bcf62ed4d2a288cdb8c9fc5d
KdC <zi24@L120-PC09>
2018-05-29 12:05:17 +0200

    Create Mens voor ut carnivoor

--- TerraTeam1/Mens.cs
+++ TerraTeam1/Mens.cs
@@ -6,37 +6,72 @@ using System.Threading.Tasks;
 
 namespace TerraTeam1
 {
-    class Mens : Carnivoor
+    public class Mens : Carnivoor
     {
+        public static List<Mens> CreateMensen(int aantal)
+        {
+            List<Mens> laMens = new List<Mens> { };
+            while (aantal > 0)
+            {
+                laMens.Add(new Mens());
+                aantal--;
+            }
+            return laMens;
+        }
+
+
         public override void Vecht(Speelveld eoSpeelveld)
         {
             if (this.TotAantStappen <= 0)
             {
+                Dier dier = null;
+
+                if ((PosY + 1 < eoSpeelveld.GrootteY)
+                    && (eoSpeelveld.Terrarium[PosX, PosY + 1].GetType() == typeof(Dier)))
+                {
+                    dier = (Dier)eoSpeelveld.Terrarium[PosX, PosY + 1];
+                }
+
                 // test if the animal at the right position is a carnivoor
-                if (PosY + 1 < eoSpeelveld.GrootteY
-                    && eoSpeelveld.Terrarium[PosX, PosY + 1] != null
+                if (dier!=null
                     && eoSpeelveld.Terrarium[PosX, PosY + 1].GetType() == typeof(Carnivoor))
                 {
                     if (this.Levenskracht > eoSpeelveld.Terrarium[PosX, PosY + 1].Levenskracht)
                     {
+                        this.Eet(eoSpeelveld, dier);
+                    }
+                    else if (this.Levenskracht < dier.Levenskracht)
+                    {
                         // add the levenskracht of the carnivoor with the carnivoor
-                        this.Levenskracht += eoSpeelveld.Terrarium[PosX, PosY + 1].Levenskracht;
-                        // remove the other carnivoor
-                        eoSpeelveld.Terrarium[PosX, PosY + 1].Delete();
-                        eoSpeelveld.Terrarium[PosX, PosY + 1] = null;   // todo: move this to the delete() of the carnivoor
-                        this.TotAantStappen++;
+                        dier.Levenskracht += this.Levenskracht;
+                        // remove the current carnivoor
+                        this.Delete();
+                        eoSpeelveld.Terrarium[PosX, PosY] = null;   // todo: move this to the delete() of the carnivoor
                     }
                     else
                     {
-                        if (this.Levenskracht < eoSpeelveld.Terrarium[PosX, PosY + 1].Levenskracht)
-                        {
-                            // add the levenskracht of the carnivoor with the carnivoor
-                            eoSpeelveld.Terrarium[PosX, PosY + 1].Levenskracht += this.Levenskracht;
-                            // remove the current carnivoor
-                            this.Delete();
-                            eoSpeelveld.Terrarium[PosX, PosY] = null;   // todo: move this to the delete() of the carnivoor
-                        }
+                        this.Eet(eoSpeelveld, dier);
                     }
+                }
+            }
+        }
+
+        public override void Eet(Speelveld eoSpeelveld, Dier dier)
+        {
+            if (this.TotAantStappen <= 0)
+            {
+                // test if the animal at the right position is a herbivoor
+                if (PosY + 1 < eoSpeelveld.GrootteY &&
+                    eoSpeelveld.Terrarium[PosX, PosY + 1] != null &&
+                    eoSpeelveld.Terrarium[PosX, PosY + 1].GetType() == typeof(Carnivoor))
+                {
+                    // add the levenskracht of the herbivoor with the carnivoor
+                    this.Levenskracht += eoSpeelveld.Terrarium[PosX, PosY + 1].Levenskracht;
+                    // remove the herbivoor
+                    eoSpeelveld.Terrarium[PosX, PosY + 1].Delete();
+                    eoSpeelveld.Terrarium[PosX, PosY + 1] = null;   // todo: move this to the delete() of the carnivoor
+                    Stap(0, 1, eoSpeelveld);
+                    this.TotAantStappen++;
                 }
             }
         }
